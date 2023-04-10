3a95011b5deb6dfb11d69b6e5b1b840ba7ff064d
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-04-10 20:03:27 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
 import { watchedBtn, queueBtn } from './js/add-watsh-que.js';
-=======
+
 
 import './js/spinner';
-=======
+
 import './js/movies-service';
 import * as myModuleCard from './js/card';
 
