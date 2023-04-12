bf6998fb5bb368f7deff6c6c02b8fa89289f2334
IgorPetrenko72 <i.v.petrenko@gmail.com>
2023-04-12 19:00:19 +0200

    draft

--- src/js/localstorage-search.js
+++ src/js/localstorage-search.js
@@ -1,45 +1,72 @@
 import Notiflix from 'notiflix';
-import { createMovieCardMarkup } from './card.js';
-import { renderPagination } from './pagination';
-
+// import { createMovieCardMarkup } from './card.js';
+// import { renderPagination } from './pagination';
+const moviesListEl = document.querySelector('.movies__list'),
+  
 
 const watchedBtn = document.querySelector('button[data-id="watched-btn"]');
 const  queueBtn = document.querySelector('button[data-id="queue-btn"]');
 
-const watched = localStorage.getItem('watchedMovies');
-const parsedWatched = JSON.parse(watched);
-
-const queue = localStorage.getItem('movies in queue');
-const parsedQueue = JSON.parse(queue);
+const parsedWatched = JSON.parse(localStorage.getItem('watchedMovies'));
+console.log(parsedWatched);
+const parsedQueue = JSON.parse(localStorage.getItem('movies in queue'));
+console.log(parsedQueue);
+console.log(parsedQueue[0].title);
 
 const searchForm = document.querySelector('.header-form__search');
+const searchInput = document.querySelector('.header-form__input');
+searchInput.addEventListener('input', (e));
 const searchQuery = searchInput.value.trim();
+function e() {console.log(searchInput.value)}
 
-searchForm.addEventListener('submit', (searchInLocalStorage))
-    searchInLocalStorage.preventDefault();
-    
-    function searchInLocalStorage() {
-        if (watchedBtn.classList.contains('header-movie-btn--active'))
-            searchInWatched();
-      if (queueBtn.classList.contains('header-movie-btn--active'))
-   searchInQueue();
-    }
 
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+        if (watchedBtn.classList.contains('header-movie-btn--active')) { searchInWatched() };
+        if (queueBtn.classList.contains('header-movie-btn--active')) { searchInQueue() };
+})
+ 
 function searchInWatched() {
-    if (watched.length === 0) {
-        Notiflix.Notify.failure('Sorry, this movie is not found');
+    if (parsedWatched.length === 0) {
+        Notiflix.Notify.failure('Sorry, Your Watched list is empty');
     }
-    else {
-        createMovieCardMarkup(movies);
-        renderPagination(page, pages, curParam);
-    }
+    const moviesHTML = queueMovies
+    .map(movie => {
+      return `
+       <li class="movies__item" data-movies="${movie.id}">
+        <div class="movies__thumb">
+         <img class="movies__img" src="${movie.posterPath}" alt="${movie.title}"/>
+        </div>
+         <p class="movies__title">${movie.title}</p>
+        <p class="movies__info">${movie.genres} | ${movie.releaseDate}</p>
+      </li>
+    `;
+    })
+    .join('');
+
+  moviesListEl.innerHTML = moviesHTML;
 }
     
 function searchInQueue() {
-    if (queue.length === 0)
-       {Notiflix.Notify.failure('Sorry, this movie is not found');
+    if (parsedQueue.length === 0) {
+        Notiflix.Notify.failure('Sorry, Your Queue list is empty');
     }
-    else
-    {createMovieCardMarkup(movies);
-    renderPagination(page, pages, curParam)}    
+    if (searchInput.value === parsedQueue[0].title) {
+        const moviesHTML = parsedQueue
+            .map(movie => {
+                return `
+       <li class="movies__item" data-movies="${movie.id}">
+        <div class="movies__thumb">
+         <img class="movies__img" src="${movie.posterPath}" alt="${movie.title}"/>
+        </div>
+         <p class="movies__title">${movie.title}</p>
+        <p class="movies__info">${movie.genres} | ${movie.releaseDate}</p>
+      </li>
+    `;
+            })
+            .join('');
+
+        moviesListEl.innerHTML = moviesHTML;
+    }
 }
--- src/library.js
+++ src/library.js
@@ -2,4 +2,4 @@
 import './js/button-queue';
 
 import './js/pagination-library';
-
+import './js/localstorage-search';
