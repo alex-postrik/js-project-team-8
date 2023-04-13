3c1ac6bf25f6907993e4fb4d6117f1cbbf51b509
IgorPetrenko72 <i.v.petrenko@gmail.com>
2023-04-13 15:29:22 +0200

    Update localstorage-search.js

--- src/js/localstorage-search.js
+++ src/js/localstorage-search.js
@@ -2,18 +2,14 @@ import Notiflix from 'notiflix';
   
 const watchedBtn = document.querySelector('button[data-id="watched-btn"]');
 const queueBtn = document.querySelector('button[data-id="queue-btn"]');
+const searchListEl = document.querySelector('.lib__list')
 
 const parsedWatched = JSON.parse(localStorage.getItem('watchedMovies'));
-// console.log(parsedWatched);
 const parsedQueue = JSON.parse(localStorage.getItem('movies in queue'));
-// console.log(parsedQueue);
-// console.log(parsedQueue[0].title);
 
 const searchForm = document.querySelector('.header-form__search');
 const searchInput = document.querySelector('.header-form__input');
 searchInput.addEventListener('input', (e) => { });
-// const searchQuery = searchInput.value.trim();
-
 
 searchForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -27,33 +23,43 @@ searchForm.addEventListener('submit', event => {
 })
  
 function searchInWatched() {
+    searchListEl.innerHTML = " ";
     if (parsedWatched.length === 0) {
         Notiflix.Notify.failure('Sorry, Your Watched list is empty');
     }
      for (const watchedTitle of parsedWatched) {
         const title = watchedTitle.title.toLowerCase();
         if (title.includes(searchInput.value))
-        {
-           console.log(watchedTitle.title);
+        { const searchedCardMarkup =
+                `<li class="movies__item" data-movies="${queueTitle.id}">
+                <div class="movies__thumb">
+                    <img class="movies__img" src="${queueTitle.posterPath}" alt="${queueTitle.title}"/>
+                </div>
+                    <p class="movies__title">${queueTitle.title}</p>
+                    <p class="movies__info">${queueTitle.genres} | ${queueTitle.releaseDate}</p>
+            </li>`;
+            searchListEl.innerHTML = searchedCardMarkup;
          }  
-        // else {
-        //     return  Notiflix.Notify.failure('Sorry, no match in Your Watched list');
-        //  } 
     }
 }
 
 function searchInQueue() {
+    searchListEl.innerHTML = " ";
     if (parsedQueue.length === 0) {
         Notiflix.Notify.failure('Sorry, Your Queue list is empty');
     }
     for (const queueTitle of parsedQueue) {
         const title = queueTitle.title.toLowerCase();
         if (title.includes(searchInput.value))
-        {
-            console.log(queueTitle.id);
+        {const searchedCardMarkup =
+                `<li class="movies__item" data-movies="${queueTitle.id}">
+                <div class="movies__thumb">
+                    <img class="movies__img" src="${queueTitle.posterPath}" alt="${queueTitle.title}"/>
+                </div>
+                    <p class="movies__title">${queueTitle.title}</p>
+                    <p class="movies__info">${queueTitle.genres} | ${queueTitle.releaseDate}</p>
+            </li>`;
+            searchListEl.innerHTML = searchedCardMarkup;
         } 
-        // else {
-        //    return Notiflix.Notify.failure('Sorry, no match in Your Queue list'); 
-        // }
       }  
 }
