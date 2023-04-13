f9035fd55f3ed59e5a568d7e613b6fb70e43f4a8
IgorPetrenko72 <i.v.petrenko@gmail.com>
2023-04-13 13:23:32 +0200

    draft

--- src/js/localstorage-search.js
+++ src/js/localstorage-search.js
@@ -1,72 +1,59 @@
 import Notiflix from 'notiflix';
-// import { createMovieCardMarkup } from './card.js';
-// import { renderPagination } from './pagination';
-const moviesListEl = document.querySelector('.movies__list'),
   
-
 const watchedBtn = document.querySelector('button[data-id="watched-btn"]');
-const  queueBtn = document.querySelector('button[data-id="queue-btn"]');
+const queueBtn = document.querySelector('button[data-id="queue-btn"]');
 
 const parsedWatched = JSON.parse(localStorage.getItem('watchedMovies'));
-console.log(parsedWatched);
+// console.log(parsedWatched);
 const parsedQueue = JSON.parse(localStorage.getItem('movies in queue'));
-console.log(parsedQueue);
-console.log(parsedQueue[0].title);
+// console.log(parsedQueue);
+// console.log(parsedQueue[0].title);
 
 const searchForm = document.querySelector('.header-form__search');
 const searchInput = document.querySelector('.header-form__input');
-searchInput.addEventListener('input', (e));
-const searchQuery = searchInput.value.trim();
-function e() {console.log(searchInput.value)}
+searchInput.addEventListener('input', (e) => { });
+// const searchQuery = searchInput.value.trim();
 
 
 searchForm.addEventListener('submit', event => {
     event.preventDefault();
-
-        if (watchedBtn.classList.contains('header-movie-btn--active')) { searchInWatched() };
-        if (queueBtn.classList.contains('header-movie-btn--active')) { searchInQueue() };
+    if (watchedBtn.classList.contains('header-movie-btn--active'))
+    { searchInWatched() };
+    if (queueBtn.classList.contains('header-movie-btn--active'))
+    { searchInQueue() };
+    if (!watchedBtn.classList.contains('header-movie-btn--active') && !queueBtn.classList.contains('header-movie-btn--active'))
+        {Notiflix.Notify.failure('Please chose Watched or Queue list'); }
+    event.target.reset();
 })
  
 function searchInWatched() {
     if (parsedWatched.length === 0) {
         Notiflix.Notify.failure('Sorry, Your Watched list is empty');
     }
-    const moviesHTML = queueMovies
-    .map(movie => {
-      return `
-       <li class="movies__item" data-movies="${movie.id}">
-        <div class="movies__thumb">
-         <img class="movies__img" src="${movie.posterPath}" alt="${movie.title}"/>
-        </div>
-         <p class="movies__title">${movie.title}</p>
-        <p class="movies__info">${movie.genres} | ${movie.releaseDate}</p>
-      </li>
-    `;
-    })
-    .join('');
-
-  moviesListEl.innerHTML = moviesHTML;
+     for (const watchedTitle of parsedWatched) {
+        const title = watchedTitle.title.toLowerCase();
+        if (title.includes(searchInput.value))
+        {
+           console.log(watchedTitle.title);
+         }  
+        // else {
+        //     return  Notiflix.Notify.failure('Sorry, no match in Your Watched list');
+        //  } 
+    }
 }
-    
+
 function searchInQueue() {
     if (parsedQueue.length === 0) {
         Notiflix.Notify.failure('Sorry, Your Queue list is empty');
     }
-    if (searchInput.value === parsedQueue[0].title) {
-        const moviesHTML = parsedQueue
-            .map(movie => {
-                return `
-       <li class="movies__item" data-movies="${movie.id}">
-        <div class="movies__thumb">
-         <img class="movies__img" src="${movie.posterPath}" alt="${movie.title}"/>
-        </div>
-         <p class="movies__title">${movie.title}</p>
-        <p class="movies__info">${movie.genres} | ${movie.releaseDate}</p>
-      </li>
-    `;
-            })
-            .join('');
-
-        moviesListEl.innerHTML = moviesHTML;
-    }
+    for (const queueTitle of parsedQueue) {
+        const title = queueTitle.title.toLowerCase();
+        if (title.includes(searchInput.value))
+        {
+            console.log(queueTitle.id);
+        } 
+        // else {
+        //    return Notiflix.Notify.failure('Sorry, no match in Your Queue list'); 
+        // }
+      }  
 }
