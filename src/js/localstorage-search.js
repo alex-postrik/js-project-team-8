792fa82b1e918502c980a215b901b9bb48f7a257
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-04-13 07:07:53 +0200

    c

--- src/js/localstorage-search.js
+++ src/js/localstorage-search.js
@@ -5,114 +5,114 @@ import Notiflix from 'notiflix';
 const moviesListEl = document.querySelector('.movies__list'),
   
 
-// import { createMovieCardMarkup } from './card.js';
-// import { renderPagination } from './pagination';
+import { createMovieCardMarkup } from './card.js';
+import { renderPagination } from './pagination';
 
 
 
-// const watchedBtn = document.querySelector('button[data-id="watched-btn"]');
-// const  queueBtn = document.querySelector('button[data-id="queue-btn"]');
+const watchedBtn = document.querySelector('button[data-id="watched-btn"]');
+const  queueBtn = document.querySelector('button[data-id="queue-btn"]');
 
 
-// const parsedWatched = JSON.parse(localStorage.getItem('watchedMovies'));
-// console.log(parsedWatched);
-// const parsedQueue = JSON.parse(localStorage.getItem('movies in queue'));
-// console.log(parsedQueue);
-// console.log(parsedQueue[0].title);
+const parsedWatched = JSON.parse(localStorage.getItem('watchedMovies'));
+console.log(parsedWatched);
+const parsedQueue = JSON.parse(localStorage.getItem('movies in queue'));
+console.log(parsedQueue);
+console.log(parsedQueue[0].title);
 
-// const searchForm = document.querySelector('.header-form__search');
-// const searchInput = document.querySelector('.header-form__input');
-// searchInput.addEventListener('input', (e));
-// const searchQuery = searchInput.value.trim();
-// function e() {console.log(searchInput.value)}
+const searchForm = document.querySelector('.header-form__search');
+const searchInput = document.querySelector('.header-form__input');
+searchInput.addEventListener('input', (e));
+const searchQuery = searchInput.value.trim();
+function e() {console.log(searchInput.value)}
 
 
-// searchForm.addEventListener('submit', event => {
-//     event.preventDefault();
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
 
-//         if (watchedBtn.classList.contains('header-movie-btn--active')) { searchInWatched() };
-//         if (queueBtn.classList.contains('header-movie-btn--active')) { searchInQueue() };
-// })
+        if (watchedBtn.classList.contains('header-movie-btn--active')) { searchInWatched() };
+        if (queueBtn.classList.contains('header-movie-btn--active')) { searchInQueue() };
+})
  
-// function searchInWatched() {
-//     if (parsedWatched.length === 0) {
-//         Notiflix.Notify.failure('Sorry, Your Watched list is empty');
-//     }
-//     const moviesHTML = queueMovies
-//     .map(movie => {
-//       return `
-//        <li class="movies__item" data-movies="${movie.id}">
-//         <div class="movies__thumb">
-//          <img class="movies__img" src="${movie.posterPath}" alt="${movie.title}"/>
-//         </div>
-//          <p class="movies__title">${movie.title}</p>
-//         <p class="movies__info">${movie.genres} | ${movie.releaseDate}</p>
-//       </li>
-//     `;
-//     })
-//     .join('');
+function searchInWatched() {
+    if (parsedWatched.length === 0) {
+        Notiflix.Notify.failure('Sorry, Your Watched list is empty');
+    }
+    const moviesHTML = queueMovies
+    .map(movie => {
+      return `
+       <li class="movies__item" data-movies="${movie.id}">
+        <div class="movies__thumb">
+         <img class="movies__img" src="${movie.posterPath}" alt="${movie.title}"/>
+        </div>
+         <p class="movies__title">${movie.title}</p>
+        <p class="movies__info">${movie.genres} | ${movie.releaseDate}</p>
+      </li>
+    `;
+    })
+    .join('');
 
-//   moviesListEl.innerHTML = moviesHTML;
-// }
+  moviesListEl.innerHTML = moviesHTML;
+}
     
-// function searchInQueue() {
-//     if (parsedQueue.length === 0) {
-//         Notiflix.Notify.failure('Sorry, Your Queue list is empty');
-//     }
-//     if (searchInput.value === parsedQueue[0].title) {
-//         const moviesHTML = parsedQueue
-//             .map(movie => {
-//                 return `
-//        <li class="movies__item" data-movies="${movie.id}">
-//         <div class="movies__thumb">
-//          <img class="movies__img" src="${movie.posterPath}" alt="${movie.title}"/>
-//         </div>
-//          <p class="movies__title">${movie.title}</p>
-//         <p class="movies__info">${movie.genres} | ${movie.releaseDate}</p>
-//       </li>
-//     `;
-//             })
-//             .join('');
+function searchInQueue() {
+    if (parsedQueue.length === 0) {
+        Notiflix.Notify.failure('Sorry, Your Queue list is empty');
+    }
+    if (searchInput.value === parsedQueue[0].title) {
+        const moviesHTML = parsedQueue
+            .map(movie => {
+                return `
+       <li class="movies__item" data-movies="${movie.id}">
+        <div class="movies__thumb">
+         <img class="movies__img" src="${movie.posterPath}" alt="${movie.title}"/>
+        </div>
+         <p class="movies__title">${movie.title}</p>
+        <p class="movies__info">${movie.genres} | ${movie.releaseDate}</p>
+      </li>
+    `;
+            })
+            .join('');
 
-//         moviesListEl.innerHTML = moviesHTML;
-//     }
+        moviesListEl.innerHTML = moviesHTML;
+    }
 
-//     const watched = localStorage.getItem('watchedMovies');
-//     const parsedWatched = JSON.parse(watched);
+    const watched = localStorage.getItem('watchedMovies');
+    const parsedWatched = JSON.parse(watched);
 
-//     const queue = localStorage.getItem('movies in queue');
-//     const parsedQueue = JSON.parse(queue);
+    const queue = localStorage.getItem('movies in queue');
+    const parsedQueue = JSON.parse(queue);
 
-//     const searchForm = document.querySelector('.header-form__search');
-//     const searchQuery = searchInput.value.trim();
+    const searchForm = document.querySelector('.header-form__search');
+    const searchQuery = searchInput.value.trim();
 
-//     searchForm.addEventListener('submit', (searchInLocalStorage))
-//     searchInLocalStorage.preventDefault();
+    searchForm.addEventListener('submit', (searchInLocalStorage))
+    searchInLocalStorage.preventDefault();
     
-//     function searchInLocalStorage() {
-//         if (watchedBtn.classList.contains('header-movie-btn--active'))
-//             searchInWatched();
-//         if (queueBtn.classList.contains('header-movie-btn--active'))
-//             searchInQueue();
-//     }
+    function searchInLocalStorage() {
+        if (watchedBtn.classList.contains('header-movie-btn--active'))
+            searchInWatched();
+        if (queueBtn.classList.contains('header-movie-btn--active'))
+            searchInQueue();
+    }
 
-//     function searchInWatched() {
-//         if (watched.length === 0) {
-//             Notiflix.Notify.failure('Sorry, this movie is not found');
-//         }
-//         else {
-//             createMovieCardMarkup(movies);
-//             renderPagination(page, pages, curParam);
-//         }
-//     }
+    function searchInWatched() {
+        if (watched.length === 0) {
+            Notiflix.Notify.failure('Sorry, this movie is not found');
+        }
+        else {
+            createMovieCardMarkup(movies);
+            renderPagination(page, pages, curParam);
+        }
+    }
     
-//     function searchInQueue() {
-//         if (queue.length === 0) {
-//             Notiflix.Notify.failure('Sorry, this movie is not found');
-//         }
-//         else {
-//             createMovieCardMarkup(movies);
-//             renderPagination(page, pages, curParam)
-//         }
-//     }
-// }
+    function searchInQueue() {
+        if (queue.length === 0) {
+            Notiflix.Notify.failure('Sorry, this movie is not found');
+        }
+        else {
+            createMovieCardMarkup(movies);
+            renderPagination(page, pages, curParam)
+        }
+    }
+}
