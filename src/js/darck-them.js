8299c2ec2e1fa3ba67cc895df76943a0152e0a92
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-04-14 07:41:21 +0200

    them

--- src/js/darck-them.js
+++ src/js/darck-them.js
@@ -1,17 +1,31 @@
-const checkbox = document.getElementById('checkbox');
-checkbox.addEventListener('change', setTheme);
+const body = document.querySelector('body');
+const check = document.querySelector('#theme-switch-toggle');
+const footer = document.querySelector('.footer');
+const footerText = document.querySelector('.footer-text');
+const footerBtn = document.querySelector('.footer-students-btn');
+// const modalTheme = document.querySelector('.modal-devs__overlay');
 
-function setTheme() {
-  if (
-    !document.body.classList.contains('dark', 'muvies__title--isDarkTheme') &&
-    checkbox.checked
-  ) {
-    document.body.classList.add('dark', 'muvies__title--isDarkTheme');
-  }
-  if (
-    document.body.classList.contains('dark', 'muvies__title--isDarkTheme') &&
-    !checkbox.checked
-  ) {
-    document.body.classList.remove('dark', 'muvies__title--isDarkTheme');
+check.addEventListener('change', onChangeTheme);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const light = Theme.LIGHT;
+const dark = Theme.DARK;
+const saveLocalStorage = localStorage.getItem('Theme');
+
+if (saveLocalStorage === 'dark-theme') {
+  check.checked = true;
+  body.classList.add('dark-theme');
+}
+function onChangeTheme() {
+  if (check.checked) {
+    localStorage.setItem('Theme', dark);
+    body.classList.add('dark-theme');
+  } else {
+    localStorage.setItem('Theme', light);
+    body.classList.remove('dark-theme');
   }
 }
